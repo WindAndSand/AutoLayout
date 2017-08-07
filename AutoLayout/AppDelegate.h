fc4c92b0ccb30481c6e845a6e8cb6c102560a0fd
WindAndSand <2711722020@qq.com>
2017-08-07 21:29:16 +0800

    创建一个AutoLayout工程

---
+++ AutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout
+//
+//  Created by 黄刚 on 2017/8/7.
+//  Copyright © 2017年 HuangGang'sMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayout/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AutoLayout
+//
+//  Created by 黄刚 on 2017/8/7.
+//  Copyright © 2017年 HuangGang'sMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
